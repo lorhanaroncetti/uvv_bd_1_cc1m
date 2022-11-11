5419d172354bf8db0eff914c3e2ab643241ddf4f
Lorhana <69554007+lorhanaroncetti@users.noreply.github.com>
2022-11-10 23:43:36 -0300

    Update cc1m_202204201_mariadb_hr.sql

--- cc1m_202204201_mariadb_hr.sql
+++ cc1m_202204201_mariadb_hr.sql
@@ -39,7 +39,6 @@ ALTER TABLE Cargos MODIFY COLUMN salario_maximo DECIMAL(8,2) COMMENT 'Se refere 
 
 
 
-
 CREATE TABLE Regioes (
                 id_regiao INT NOT NULL,
                 nome VARCHAR(50),
@@ -50,11 +49,8 @@ CREATE UNIQUE INDEX regiões_idx
  ON Regioes(nome);
 
 ALTER TABLE Regioes COMMENT 'Tabela regiões, que armazena as informações das regiões. É uma tabela do tipo 1:N.';
-
 ALTER TABLE Regioes MODIFY COLUMN id_regiao INTEGER COMMENT 'Se refere ao código identificador atribuído a uma região a qual o país onde está localizado um escritório ou facilidade da empresa pertence. Funciona como chave primária da tabela regiões.';
-
 ALTER TABLE Regioes MODIFY COLUMN nome VARCHAR(50) COMMENT 'Se refere ao nome de uma região a qual o país onde está localizado um escritório ou facilidade da empresa pertence';
-
 
 
 
@@ -69,13 +65,9 @@ CREATE UNIQUE INDEX países_idx
  ON Paises( nome_pais );
 
 ALTER TABLE Paises COMMENT 'Tabela que informa nome e código de países nos quais existem escritórios e também uma FK (id_pais).';
-
 ALTER TABLE Paises MODIFY COLUMN id_pais CHAR(2) COMMENT 'Se refere ao código identificador atribuído a um país onde está localizado um escritório ou facilidade da empresa. Funciona como chave primária da tabela países.';
-
 ALTER TABLE Paises MODIFY COLUMN nome_pais VARCHAR(50) COMMENT 'Se refere ao nome do país onde está localizado um escritório ou facilidade da empresa. Funciona como índice único';
-
 ALTER TABLE Paises MODIFY COLUMN id_regiao INT COMMENT 'É uma chave estrangeira do tipo integer (numero não fracionário). Como toda FK, esta coluna não pode ser deixada com o valor nulo.';
-
 
 
 
@@ -90,21 +82,14 @@ CREATE TABLE Localizaes (
                 PRIMARY KEY (id_localizacao));
                
 ALTER TABLE Localizaes COMMENT 'TTabela localizações, que armazena os endereços dos escritórios e facilidades da empresa.';
-
 ALTER TABLE Localizaes MODIFY COLUMN id_localizacao INT COMMENT 'Se refere ao código identificador atribuído a uma localização; onde está localizado um escritório. Funciona como uma PK da tabela localizações.';
-
 ALTER TABLE Localizaes MODIFY COLUMN endereco VARCHAR(50) COMMENT 'Se refere ao endereço, constituído de número e logradouro, de um escritório.';
-
 ALTER TABLE Localizaes MODIFY COLUMN cidade VARCHAR(50) COMMENT 'Se refere a cidade onde está localizado um escritório.';
-
 ALTER TABLE Localizaes MODIFY COLUMN cep VARCHAR(8) COMMENT 'Se refere ao CEP da localização de um escritório ou facilidade da empresa, coloquei como not null para facilitar a localização do endereço.';
-
 ALTER TABLE Localizaes MODIFY COLUMN uf VARCHAR(25) COMMENT 'Se refere ao estado (por extenso) onde está localizado o escritório.';
-
 ALTER TABLE Localizaes MODIFY COLUMN id_pais CHAR(2) COMMENT 'Se refere ao código identificador atribuído a um país onde está localizado um escritório. Funciona como FK para a tabela países.';
 
-               
-               
+                             
                    
                                            
 CREATE TABLE Departamentos (
@@ -117,13 +102,9 @@ CREATE UNIQUE INDEX departamentos_idx
  ON Departamentos( nome );
 
 ALTER TABLE Departamentos COMMENT 'Tabela departamentos, que armazena os dados  dos  departamentos da empresa.';
-
 ALTER TABLE Departamentos MODIFY COLUMN id_departamento INT COMMENT 'Se refere ao código identificador atribuído a um departamento. Funciona como uma PK da tabela departamentos.';
-
 ALTER TABLE Departamentos MODIFY COLUMN nome VARCHAR(50) COMMENT 'Se refere ao nome atribuído a um departamento. Funciona como índice único.';
-
 ALTER TABLE Departamentos MODIFY COLUMN id_localizacao INT COMMENT 'Se refere ao código identificador da localização a qual o departamento pertence. Funciona como FK para a tabela localizações.';
-
 
 
 
@@ -154,27 +135,16 @@ CREATE UNIQUE INDEX empregados_idx
  ON Empregados( email );
 
 ALTER TABLE Empregados COMMENT 'Tabela empregados, que armazena os dados dos empregados da empresa';
-
 ALTER TABLE Empregados MODIFY COLUMN id_empregados INT COMMENT 'Se refere ao código identificador atribuído a um empregado. Funciona como chave primária da tabela empregados.';
-
 ALTER TABLE Empregados MODIFY COLUMN nome_empregado VARCHAR(75) COMMENT 'Se refere a nome completo do funcionário.';
-
 ALTER TABLE Empregados MODIFY COLUMN email VARCHAR(35) COMMENT 'Se refere a parte inicial do email do empregado.';
-
 ALTER TABLE Empregados MODIFY COLUMN telefone VARCHAR(20) COMMENT 'Se refere ao telefone do empregado. Deve ser inserido o código do país e do estado (sem espaço e sem caracteres especiais).';
-
 ALTER TABLE Empregados MODIFY COLUMN data_contratacao DATE COMMENT 'Se refere a data em que o cargo atual foi atribuído ao empregado';
-
 ALTER TABLE Empregados MODIFY COLUMN id_cargo VARCHAR(10) COMMENT 'Se refere ao código identificador atribuído ao cargo atual do empregado. Funciona como FK para a tabela cargos.';
-
 ALTER TABLE Empregados MODIFY COLUMN salario DECIMAL(8, 2) COMMENT 'Se refere ao salário atual do empregado, em reais (sem R$). Deve ser maior ou igual a 1.212.';
-
 ALTER TABLE Empregados MODIFY COLUMN comissao DECIMAL(4, 2) COMMENT 'Se refere a porcentagem de comissão atribuída ao empregado. Sendo que, apenas funcionários do departamento de vendas são elegíveis para comissões. Não pode ser maior que 30. Não pode ser negativa.';
-
 ALTER TABLE Empregados MODIFY COLUMN id_departamento INT COMMENT 'Se refere ao código identificador atribuído ao departamento que o empregado pertence.  Funciona como chave estrangeira para a tabela departamentos.';
-
 ALTER TABLE Empregados MODIFY COLUMN id_supervisor INT COMMENT 'Se refere ao empregado que atua como supervisor direto do empregado em questão. Funciona como FK para a própria tabela empregados autorelacionamento.';
-
 
 
 
@@ -185,15 +155,12 @@ CREATE TABLE Supervisao (
                 PRIMARY KEY (id_departamento, id_gerente));
                
 ALTER TABLE Supervisao COMMENT 'Tabela supervisao, que armazena as informações referentes a qual empregado gerencia qual departamento. Possui PFK para a tabela funcionários.';
-
 ALTER TABLE Supervisao MODIFY COLUMN id_departamento INT COMMENT 'Se refere ao código identificador atribuído ao departamento que o empregado trabalha como gerente. Junto com a coluna id_gerente, funciona como chave primária composta da tabela trabalha_em. Também funciona como chave  estrangeira para a tabela departamentos.';
-
 ALTER TABLE Supervisao MODIFY COLUMN id_gerente INT COMMENT 'Se refere ao código identificador atribuído a um empregado que gerencia um departamento. A chave primária vai garantir que este valor não seja repetido ou confundido com a identificação de outro funcionário.';
 
                
                
-               
-
+              
 CREATE TABLE Historico_Cargos (
                 data_inicial DATE NOT NULL,
                 data_final DATE NOT NULL,
@@ -207,15 +174,10 @@ ALTER TABLE Historico_Cargos add
 			check (data_inicial < data_final);
 				
 ALTER TABLE Historico_Cargos COMMENT 'Tabela histórico_cargos, que armazena o histórico de cargos de um empregado. Uma linha nova deve ser inserida para cada alteração feita no cadastro de um empregado, como por exemplo uma transferência de departamento ou de cargo.';
-
 ALTER TABLE Historico_Cargos MODIFY COLUMN data_inicial DATE COMMENT 'Se refere ao código identificador atribuído a um histórico. Funciona como chave primária da tabela historico_cargos. Deve ser menor que a data final';
-
 ALTER TABLE Historico_Cargos MODIFY COLUMN data_final DATE COMMENT 'Se refere ao último dia em que o empregado exerceu o cargo. Deve ser maior que a data_inicial.';
-
 ALTER TABLE Historico_Cargos MODIFY COLUMN id_cargo VARCHAR(10) COMMENT 'Se refere ao código identificador atribuído ao cargo do empregado. Funciona como chave estrangeira para a tabela cargos.';
-
 ALTER TABLE Historico_Cargos MODIFY COLUMN id_empregados INT COMMENT 'Se refere ao código identificador atribuído a um empregado.  Funciona como chave estrangeira para a tabela empregados.';
-
 ALTER TABLE Historico_Cargos MODIFY COLUMN id_departamento INT COMMENT 'Se refere ao código identificador atribuído ao departamento ao qual o empregado pertence. Funciona como chave estrangeira para a tabela departamentos.';
             
          
